7fe8d988f32f3e9b4af0efee713ceaa437870b05
Wilmer Choxom PC <lorwilm@gmail.com>
2024-03-09 03:10:02 -0600

    agregando encriptado a mi estilo

--- scripts/script.js
+++ scripts/script.js
@@ -4,12 +4,14 @@ let letra="";
 let variableMomentanea;
 let longitud=0;
 let descuento=1;
-let palabrareordenada="";
+let palabrareordenada=[];
 let historialEncriptando =[];
 let HistorialEncriptado = [];
-
+let shoWords="";
+let newWord ="";    
 //funciton replace level alura
 function aluraEnript(){
+    shoWords=document.getElementById('text-encripting').id;
     let newPalabra="";
     palabra = document.getElementById('text-for-encript').value;
     newPalabra = palabra.replace(/e/g, 'enter');
@@ -19,10 +21,21 @@ function aluraEnript(){
     newPalabra = newPalabra.replace(/u/g, 'ufat');
     console.log(newPalabra);
     guardarHistorial();
+    addElementWord(shoWords, newPalabra);
     palabra = "";
+
+}
+//funcion replace wilmer style lv1
+function replaceWilmerStyle(){
+    shoWords=document.getElementById('text-encripting').id;
+    constructorOtherMethod();
+    newWord = palabrareordenada.join('');
+    addElementWord(shoWords, newWord);
 }
 
+
 function desencriptarAluraEnript(){
+    shoWords=document.getElementById('text-encripting').id;
     palabra = document.getElementById('text-for-encript').value;
     let lastWord = "";
     lastWord = palabra.replace(/ufat/g, 'u');
@@ -32,36 +45,33 @@ function desencriptarAluraEnript(){
     lastWord = lastWord.replace(/enter/g, 'e');
     console.log(lastWord);
     guardarHistorial();
+    addElementWord(shoWords, lastWord);
     palabra="";
     return lastWord;
     
 }
 
 //funcion para encriptar nivel wilmer
-function encriptWilmerLvOne(){
+function constructorOtherMethod(){
     cadena=[];
     palabra = document.getElementById('text-for-encript').value;
     longitud = palabra.length
-    parametro = palabra.length
+    //parametro = palabra.length
     console.log("longitud" +longitud);
-
     console.log(palabra[palabra.length-1]);
 
     //cicle, this is for make a array with letters of cadena variable
     while (longitud>0){
         longitud --;
-        
         letra=palabra[palabra.length-descuento];
         console.log("letra palabra" + palabra[palabra.length-descuento]);
         cadena.push(letra);
         console.log(letra);
         console.log("cadena "+ cadena);
         descuento ++;
-        
         console.log(descuento);
     }
     descuento = 1;
-
     palabrareordenada = cadena.map(letraA=> letraA.toLocaleLowerCase() === 'a' ? '56a565f8a8ese42' :letraA);
     palabrareordenada = palabrareordenada.map(letraB => letraB.toLowerCase() === 'b' ? '99s9f8r5g5h5j5r' :letraB);
     palabrareordenada = palabrareordenada.map(letraC => letraC.toLowerCase() === 'c' ? '46sd79s80000471' :letraC);
@@ -90,8 +100,12 @@ function encriptWilmerLvOne(){
     palabrareordenada = palabrareordenada.map(letraN => letraN.toLowerCase() === 'z' ? '5f6dh78dt4hd684' :letraN);
     //case for languages with ñ letter
     palabrareordenada = palabrareordenada.map(letraÑ => letraÑ.toLowerCase() === 'ñ' ? '46546af486e4f8w' :letraÑ);
+    //empty value
+    palabrareordenada = palabrareordenada.map(spaceWord => spaceWord === " " ? '00100577770077w' :spaceWord);
 
     console.log(palabrareordenada);
+    //print new array but convert array to string
+    
     guardarHistorial();
     
     console.log
@@ -107,4 +121,38 @@ function guardarHistorial(){
     
 
 }
+
+function encriptationGeneral(){
+    //validation for a type of encriptation
+    let comprobation = document.getElementById('select-level-encript').value;
+    if (comprobation == 'nivelAlura'){
+        aluraEnript();
+    }else if (comprobation == 'nivelWilmer'){
+        replaceWilmerStyle();
+    }else if (comprobation == 'nivelWilmerLvTwo'){
+        
+    }
+}
+
+function desencriptarGeneral(){
+    let comprobation = document.getElementById('select-level-encript').value;
+    if (comprobation == 'nivelAlura'){
+        desencriptarAluraEnript();
+    }else if(comprobation == 'nivelWilmer'){
+        
+    }else if(comprobation == 'nivelWilmerLvTwo'){
+
+    }
+}
+
+function addElementWord(element, text){
+    let addWord = document.getElementById(element);
+    addWord.value= text;
+    return ;
+}
+
+
+
+
+
 
